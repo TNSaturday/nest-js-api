1b10e03d2d4869836225badd0c4b8c289bc4178a
TNsaturday <tnsaturday@gmail.com>
2022-04-11 17:51:36 +0300

    Add auth guard for users/me route

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -2,11 +2,16 @@ import {PassportStrategy} from "@nestjs/passport";
 import {ExtractJwt, Strategy} from "passport-jwt";
 import {Injectable} from "@nestjs/common";
 import {ConfigService} from "@nestjs/config";
+import {PrismaService} from "../../prisma/prisma.service";
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
+export class JwtStrategy extends PassportStrategy(
+    Strategy,
+    'jwt'
+) {
     constructor(
-        private config: ConfigService
+        config: ConfigService,
+        private prisma: PrismaService,
     ) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -15,7 +20,14 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         });
     }
 
-    async validate() {
-        return;
+    async validate(payload: { sub: number, email: string }) {
+        const user = await this.prisma.user.findUnique({
+            where: {
+                id: payload.sub,
+            }
+        });
+
+        delete user.hash;
+        return user;
     }
 }
