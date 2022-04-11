ad21d192b1e4056af779acbdf27bd1bbfc2a8965
TNsaturday <tnsaturday@gmail.com>
2022-04-11 16:21:22 +0300

    Add JWT Strategy to decorate all routes

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,21 @@
+import {PassportStrategy} from "@nestjs/passport";
+import {ExtractJwt, Strategy} from "passport-jwt";
+import {Injectable} from "@nestjs/common";
+import {ConfigService} from "@nestjs/config";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(
+        private config: ConfigService
+    ) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: true,
+            secretOrKey: config.get('JWT_SECRET'),
+        });
+    }
+
+    async validate() {
+        return;
+    }
+}
