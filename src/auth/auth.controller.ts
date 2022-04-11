dba9436b30963b1b9b8126fde694dd33f857a5c9
TNsaturday <tnsaturday@gmail.com>
2022-04-11 18:13:31 +0300

    Change HTTP response code to 200 on successfull signin

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
-import {Controller, Post, Body} from '@nestjs/common';
-import {AuthService} from "./auth.service";
-import {AuthDto} from "./dto";
+import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
+import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
 
 @Controller('auth')
 export class AuthController {
@@ -11,6 +11,7 @@ export class AuthController {
         return this.authService.signup(dto);
     }
 
+    @HttpCode(HttpStatus.OK)
     @Post('signin')
     signin(@Body() dto: AuthDto) {
         return this.authService.signin(dto);
